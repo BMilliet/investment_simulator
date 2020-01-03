1b02419eec7c468be281c826618fbb125ebadee3
Bruno <brunomilliet89@gmail.com>
2020-01-03 17:38:36 -0300

    init

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '10.0'
+
+target 'investment_simulator' do
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'SwiftLint'
+
+  target 'investment_simulatorTests' do
+    inherit! :search_paths
+    pod 'Quick'
+    pod 'Nimble'
+  end
+end
---
+++ investment_simulator/SceneDelegate.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+
+  func scene(_ scene: UIScene,
+             willConnectTo session: UISceneSession,
+             options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+
+    let rootView = ViewController()
+    let rootNavigationController = UINavigationController(rootViewController: rootView)
+
+    window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+    window?.windowScene = windowScene
+    window?.rootViewController = rootNavigationController
+    window?.makeKeyAndVisible()
+
+    guard (scene as? UIWindowScene) != nil else { return }
+  }
+}
