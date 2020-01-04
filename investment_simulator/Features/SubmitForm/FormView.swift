237b1fc9a59795cd1c682378868f1f0b2f07d20b
Bruno <brunomilliet89@gmail.com>
2020-01-04 16:09:15 -0300

    auto layour with scroll

--- investment_simulator/Features/SubmitForm/FormView.swift
+++ investment_simulator/Features/SubmitForm/FormView.swift
@@ -1,62 +1,54 @@
 import UIKit
 
-class FormView: UIViewController {
-  let scrollView = UIScrollView()
-  let valueAmountForm = FormViewUIContent.valueAmountForm()
-  let dateForm = FormViewUIContent.dateForm()
-  let cdiPercentForm = FormViewUIContent.CDIPercentForm()
+class FormView: UIViewController, CustomizableByClosure {
+  private let valueAmountForm = FormViewUIContent.valueAmountForm()
+  private let cdiPercentForm = FormViewUIContent.CDIPercentForm()
+  private let dateForm = FormViewUIContent.dateForm()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    setupScrollView()
-    setupLayoutComponents()
     view.backgroundColor = Colors.whiteColor
+    addUIComponents()
+    setupScrollConstraints()
+    setupStackConstraints()
   }
 
-  private func setupLayoutComponents() {
-    setupValueAmountForm()
-    setupDateForm()
-    setupCDIPercentForm()
+  let scrollView: UIScrollView = {
+      let scrollView = UIScrollView()
+      return scrollView
+  }()
+
+  lazy var stack = customInit(UIStackView()) { stack in
+    stack.axis = .vertical
+    stack.spacing = 20
   }
 
-  private func setupValueAmountForm() {
-    scrollView.addSubview(valueAmountForm)
-    valueAmountForm.anchor(top: scrollView.safeAreaLayoutGuide.topAnchor,
-                           leading: scrollView.safeAreaLayoutGuide.leadingAnchor,
-                           trailing: scrollView.safeAreaLayoutGuide.trailingAnchor,
-                           padding: UIEdgeInsets(top: Dimens.spacing60,
-                                                 left: Dimens.spacing16,
-                                                 bottom: Dimens.zero,
-                                                 right: -Dimens.spacing16))
+  lazy var spacer = customInit(UIView()) { spacer in
+    spacer.backgroundColor = .clear
+    spacer.size(height: 20)
   }
 
-  private func setupDateForm() {
-    scrollView.addSubview(dateForm)
-    dateForm.anchor(top: valueAmountForm.safeAreaLayoutGuide.bottomAnchor,
-                    leading: scrollView.safeAreaLayoutGuide.leadingAnchor,
-                    trailing: scrollView.safeAreaLayoutGuide.trailingAnchor,
-                    padding: UIEdgeInsets(top: Dimens.spacing60,
-                                          left: Dimens.spacing16,
-                                          bottom: Dimens.zero,
-                                          right: -Dimens.spacing16))
-  }
-
-  private func setupCDIPercentForm() {
-    scrollView.addSubview(cdiPercentForm)
-    cdiPercentForm.anchor(top: dateForm.safeAreaLayoutGuide.bottomAnchor,
-                          leading: scrollView.safeAreaLayoutGuide.leadingAnchor,
-                          trailing: scrollView.safeAreaLayoutGuide.trailingAnchor,
-                          padding: UIEdgeInsets(top: Dimens.spacing60,
-                                                left: Dimens.spacing16,
-                                                bottom: Dimens.zero,
-                                                right: -Dimens.spacing16))
-  }
-
-  private func setupScrollView() {
+  private func addUIComponents() {
     view.addSubview(scrollView)
+    scrollView.addSubview(stack)
+    stack.addArrangedSubview(spacer)
+    stack.addArrangedSubview(valueAmountForm)
+    stack.addArrangedSubview(dateForm)
+    stack.addArrangedSubview(cdiPercentForm)
+  }
+
+  private func setupScrollConstraints() {
     scrollView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
                       leading: view.safeAreaLayoutGuide.leadingAnchor,
                       bottom: view.safeAreaLayoutGuide.bottomAnchor,
                       trailing: view.safeAreaLayoutGuide.trailingAnchor)
+  }
+
+  private func setupStackConstraints() {
+    stack.anchor(top: scrollView.topAnchor,
+                 leading: scrollView.leadingAnchor,
+                 bottom: scrollView.bottomAnchor,
+                 trailing: scrollView.trailingAnchor)
+    stack.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
   }
 }
