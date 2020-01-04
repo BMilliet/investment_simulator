dc6f8db2153abea418456494ecb848ae7de01b03
Bruno <brunomilliet89@gmail.com>
2020-01-04 09:08:03 -0300

    refactor

--- investment_simulatorTests/ApiRouterTest.swift
+++ investment_simulatorTests/ApiRouterTest.swift
@@ -10,8 +10,8 @@ class ApiRouterTest: QuickSpec {
 
     let apiRouter = ApiRouter()
 
-    describe("#getMoviesEndPoint") {
-      context("build movies endPoint") {
+    describe("#getSimulationEndPoint") {
+      context("build simulation endPoint") {
         let expected = "https://api-simulator-calc.easynvest.com.br/calculator/simulate?investedAmount=32323.0&index=CDI&rate=100.0&isTaxFree=false&maturityDate=2023-03-03"
 
         let subject = apiRouter.getSimulationEndPoint(investedAmountValue: 32323.0, rateValue: 100, maturityDateValue: "2023-03-03")!.absoluteString
