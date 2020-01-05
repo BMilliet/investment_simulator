f60574d759eea6ec44cae93e9532337f5e02487d
Bruno <brunomilliet89@gmail.com>
2020-01-05 09:57:15 -0300

    textField mask done

--- investment_simulator/Helpers/MaskFormatter.swift
+++ investment_simulator/Helpers/MaskFormatter.swift
@@ -3,48 +3,70 @@ import UIKit
 struct MaskFormatter {
   func formatDateMask(_ baseText: String, _ char: String) -> String {
     var finalText = baseText
-
-    if char == "" { return "" }
-
-    if finalText.count == 10 {
-      return finalText
-    }
-
+    let maskValue = Character("/")
+    deletionHandle(&finalText, char, maskValue)
+    if isMaxLength(baseText, 10) { return finalText }
     finalText.append(char)
-
-    if finalText.count == 2 || finalText.count == 5 {
-      finalText.append("/")
-    }
+    appendMaskAt(&finalText, especial: maskValue, index: 2)
+    appendMaskAt(&finalText, especial: maskValue, index: 5)
     return finalText
   }
 
   func percentMask(_ baseText: String, _ char: String) -> String {
     var finalText = baseText
-
-    if char == ""{
-      finalText.remove(at: finalText.index(before: finalText.endIndex))
-      return finalText
-    }
-
-    if finalText.count == 3 {
-      return finalText
-    }
+    let maskValue = Character(".")
+    deletionHandle(&finalText, char, maskValue)
+    if isMaxLength(baseText, 25) { return finalText }
     finalText.append(char)
+    appendMaskAt(&finalText, especial: maskValue, index: 3)
     return finalText
   }
 
   func amountMask(_ baseText: String, _ char: String) -> String {
     var finalText = baseText
-
-    if char == ""{
-      finalText.remove(at: finalText.index(before: finalText.endIndex))
+    if isBackSpace(char) {
+      deleteLastChar(&finalText)
       return finalText
     }
-
-    if finalText.count == 20 {
-      return finalText
-    }
+    if isMaxLength(baseText, 25) { return finalText }
     finalText.append(char)
     return finalText
+  }
+
+  private func appendMaskAt(_ baseText: inout String,
+                            especial: Character,
+                            index: Int) {
+    if baseText.count == index { baseText.append(especial) }
+  }
+
+  private func isMaxLength(_ baseText: String,
+                           _ max: Int) -> Bool {
+    return baseText.count == max
+  }
+
+  private func isBackSpace(_ char: String) -> Bool {
+    return char == ""
+  }
+
+  private func deleteLastChar(_ baseText: inout String) {
+    baseText.remove(at: baseText.index(before: baseText.endIndex))
+  }
+
+  private func deleteTwice(_ baseText: inout String) {
+    deleteLastChar(&baseText)
+    deleteLastChar(&baseText)
+  }
+
+  private func isMaskValue(_ baseText: String, _ maskValue: Character) -> Bool {
+    return baseText.last == maskValue
+  }
+
+  private func deletionHandle(_ baseText: inout String, _ char: String, _ maskValue: Character) {
+    if isBackSpace(char) {
+      isMaskValue(baseText, maskValue) ?
+        deleteTwice(&baseText) :
+        deleteLastChar(&baseText)
+    }
+    return
   }
 }
